cf315dd2f3f43bb4cc2f07f55f77ef118f2d3bb7
DruDruDru <borisrasgard251@gmail.com>
2024-01-19 22:20:34 +0700

    Каркас

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
+Vue.component('column', {
+    props: {
+        cards: {
+            type: Array,
+            required: true,
+        }  
+    },
+    template: `
+        <div class="column">
+            <card v-for="card in cards"></card>
+        </div>
+    `
+})
+
+Vue.component('card', {
+    template: `
+        <p>Test card</p>
+        <span>test</span>
+    `
+})
+
 let app = new Vue({
     el: '#app',
     data: {
-        test: 'test'
+        cardsInPlan: ['first', 'second', 'third'],
+        cardsInWork: ['first', 'second', 'third'],
+        cardsInTest: ['first', 'second', 'third'],
+        cardsInComplete: ['first', 'second', 'third'],
     }
 })
