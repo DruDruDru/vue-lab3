d6a7fcacc18675209098d598b6af905547b54a78
DruDruDru <borisrasgard251@gmail.com>
2024-01-20 11:09:46 +0700

    Удаление карточки

--- js/main.js
+++ js/main.js
@@ -11,9 +11,16 @@ Vue.component('column', {
                 v-for="card in cards"
                 :card="card"
                 :key="card.id"
+                @card-to-delete="deleteCard"
             ></card>
         </div>
-    `
+    `,
+    methods: {
+        deleteCard(card) {
+            idx = this.cards.indexOf(card)
+            this.cards = this.cards.filter((value, index) => index !== idx)
+        }
+    }
 })
 
 Vue.component('card', {
@@ -29,20 +36,37 @@ Vue.component('card', {
             <p>{{ card.title }}</p>
             <p>{{ card.description }}</p>
             <p>{{ card.deadline }}</p>
+            <div>
+                <input type="submit" @click="cardToDelete(card)" value="X" />
+            </div>
         </div>
-    `
+    `,
+    methods: {
+        cardToDelete(card) {
+            this.$emit('card-to-delete', card);
+        }
+    }
 })
 
 let app = new Vue({
     el: '#app',
     data: {
-        cardsInPlan: [{
-            id: 0,
-            dateOfCreate: new Date(),
-            title: 'Сделать лабу',
-            description: 'Нужно сделать лабороторную работу на Vue.js',
-            deadline: new Date(),
-        }],
+        cardsInPlan: [
+            {
+                id: 0,
+                dateOfCreate: new Date(),
+                title: 'Сделать лабу',
+                description: 'Нужно сделать лабороторную работу на Vue.js',
+                deadline: new Date(),
+            },
+            {
+                id: 1,
+                dateOfCreate: new Date(),
+                title: 'Сделать лабуi',
+                description: 'Нужно сделать лабороторную работу на Vue.js',
+                deadline: new Date(),
+            },
+        ],
         cardsInWork: [],
         cardsInTest: [],
         cardsInComplete: [],
