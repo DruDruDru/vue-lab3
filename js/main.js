4614851b65adb91bc7824905eda2fe6fb8221a6e
DruDruDru <borisrasgard251@gmail.com>
2024-01-21 19:21:41 +0700

    Добавление редактирования

--- js/main.js
+++ js/main.js
@@ -12,6 +12,11 @@ Vue.component('column', {
             required: true,
         }  
     },
+    data() {
+        return {
+            clickOnAdd: false,
+        }
+    },
     template: `
         <div class="column">
             <card 
@@ -19,22 +24,22 @@ Vue.component('column', {
                 :card="card"
                 :key="card.id"
             ></card>
-            <button @click="onClick" v-show="!click" v-if="addable">Добавить</button>
+            <button 
+                @click="onClick" 
+                v-show="!clickOnAdd"
+                v-if="addable"
+            >Добавить</button>
             <add-card 
-                v-show="click"
-                @on-submit-for-click="onClick"
+                v-if="addable"
+                @on-submit-for-create-form="onClick"
+                v-show="clickOnAdd"
             ></add-card>
         </div>
     `,
-    data() {
-        return {
-            click: false,
-        }
-    },
     methods: {
         onClick() {
-            if (this.click) this.click = false
-            else this.click = true
+            if (this.clickOnAdd) this.clickOnAdd = false;
+            else this.clickOnAdd = true;
         }
     }
 })
@@ -47,26 +52,66 @@ Vue.component('card', {
         }
     },
     template: `
-        <div class="card">
-            <p>{{ card.dateOfCreate }}</p>
-            <p>{{ card.title }}</p>
-            <p>{{ card.description }}</p>
-            <p>{{ card.deadline }}</p>
-            <div class="button-container">
-                <input type="submit" @click="cardToDelete" value="Удалить" />
+        <div>
+            <div class="card" v-show="!click">
+                <p>{{ card.dateOfCreate }}</p>
+                <p>{{ card.title }}</p>
+                <p class="desciption">{{ card.description }}</p>
+                <p>Дэдлайн: {{ card.deadline }}</p>
+                <div class="button-container">
+                    <input type="submit" @click="cardToEdit" value="Редактировать" />
+                    <input type="submit" @click="cardToDelete" value="Удалить" />
+                </div>
             </div>
+            
+            <form @submit.prevent="onSubmit" v-show="click" class="update">
+                <p>
+                    <label for="title">Заголовок:</label>
+                    <input type="text" required maxlength=25 v-model="card.title" id="title" />
+                </p>
+                <p>
+                    <label for="description">Описание:</label>
+                    <textarea required id="description" maxlength=100 v-model="card.description"></textarea>
+                </p>
+                <p>
+                    <label for="deadline">Дедлайн:</label>
+                    <input type="date" required id="deadline" v-model="card.deadline" />
+                </p>
+                <p>
+                    <input type="submit" value="Сохранить" />
+                </p>
+            </form>
         </div>
     `,
+    data() {
+        return {
+            click: false,
+        }
+    },
+    mounted() {
+        let date = this.card.deadline
+        let year = date.getFullYear()
+        let month = (date.getMonth() + 1).toString().padStart(2, '0');
+        let day = (date.getDate()).toString().padStart(2, '0');
+        this.card.deadline = `${year}-${month}-${day}`
+    },
     methods: {
+        onSubmit() {
+            this.click=false
+        },
         cardToDelete() {
             eventBus.$emit('card-to-delete', this.card);
         },
-    }
+        cardToEdit() {
+            this.click = true;
+            eventBus.$emit('card-to-edit', this.card);
+        }
+    },
 })
 
 Vue.component('add-card', {
     template: `
-        <form @submit.prevent="onSubmit">
+        <form @submit.prevent="onSubmit" class="add-card">
             <p>
                 <label for="title">Заголовок:</label>
                 <input type="text" required maxlength=25 v-model="title" id="title" />
@@ -80,7 +125,7 @@ Vue.component('add-card', {
                 <input type="date" required id="deadline" v-model="deadline" />
             </p>
             <p>
-                <input type="submit" value="Отправить" />
+                <input type="submit" value="Сохранить" />
             </p>
         </form>
     `,
@@ -90,7 +135,6 @@ Vue.component('add-card', {
             title: null,
             description: null,
             deadline: null,
-            click: false
         }
     },
     methods: {
@@ -99,14 +143,14 @@ Vue.component('add-card', {
                 dateOfCreate: new Date(),
                 title: this.title,
                 description: this.description,
-                deadline: this.deadline
+                deadline: new Date(this.deadline)
             }
             eventBus.$emit('on-submit', card)
             this.dateOfCreate = null
             this.title = null,
             this.description = null,
             this.deadline = null
-            this.$emit('on-submit-for-click')
+            this.$emit('on-submit-for-create-form')
         }
     }
 })
@@ -120,14 +164,14 @@ let app = new Vue({
                 dateOfCreate: new Date(),
                 title: 'Сделать лабу',
                 description: 'Нужно сделать лабороторную работу на Vue.js',
-                deadline: new Date(),
+                deadline: new Date('2024-10-12'),
             },
             {
                 id: 1,
                 dateOfCreate: new Date(),
                 title: 'Сделать лабуi',
                 description: 'Нужно сделать лабороторную работу на Vue.js',
-                deadline: new Date(),
+                deadline: new Date('2024-10-12'),
             },
         ],
         cardsInWork: [],
@@ -143,6 +187,9 @@ let app = new Vue({
         }),
         eventBus.$on('on-submit', card => {
             this.cardsInPlan.push(card)
+        }),
+        eventBus.$on('card-to-edit', card => {
+
         })
     },
 })
